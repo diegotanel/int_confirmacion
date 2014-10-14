be12c25cdfd6f0964e3bc829cd4263618442082f
Martin <pereira.martin@gmail.com>
2014-10-14 11:57:45 -0300

    Se agrego el mmodelo de elenco_en_gira con sus respectivas vistas

--- app/controllers/elencos_en_gira_controller.rb
+++ app/controllers/elencos_en_gira_controller.rb
@@ -4,6 +4,7 @@ class ElencosEnGiraController < ApplicationController
   def index
     @formulario = Formulario.find_by_id(params[:formulario_id])
     @integrantes = []
+    @formulario.create_elenco_en_gira() unless @formulario.elenco_en_gira
     @actores = @formulario.elenco_en_gira.integrantes_de_elenco_en_gira.where(type: 'Actor')
     @directores = @formulario.elenco_en_gira.integrantes_de_elenco_en_gira.where(type: 'Director')
     @tecnicos = @formulario.elenco_en_gira.integrantes_de_elenco_en_gira.where(type: 'Tecnico')
