a13b2d6dbde3904a39868f9839ae7c13ab28a9ab
Diego Tanel <diego.tanel@tecneg.com>
2014-10-28 21:57:34 -0300

    Se comento la clase generador_de_remito para poder hacer el push a heroku

--- app/models/generador_de_remito.rb
+++ app/models/generador_de_remito.rb
@@ -1,15 +1,15 @@
 #encoding: utf-8
-require '../lib/odf-report'
+# require '../lib/odf-report'
 
-report = ODFReport::Report.new("./plantillas/REMITO_INT_PRESENTA.odt") do |r|
+# report = ODFReport::Report.new("./plantillas/REMITO_INT_PRESENTA.odt") do |r|
 
-  r.add_field("FECHA_DE_IMPRESION_DEL_REMITO", "14/04/2014 12:00")
-  r.add_field("NOMBRE_DEL_ESPECTACULO", "espectáculo fruta")
-  r.add_field("PROVINCIA", "Jujuy")
-  r.add_field("REGION", "NOA")
-  r.add_field("NUMERO_DE_TRAMITE", "123")
-  r.add_field("FECHA_DE_TRAMITE", "12/04/2014 15:00")
-  r.add_field("DATOS_DEL_RESPONSABLE", "fruta")
-end
+#   r.add_field("FECHA_DE_IMPRESION_DEL_REMITO", "14/04/2014 12:00")
+#   r.add_field("NOMBRE_DEL_ESPECTACULO", "espectáculo fruta")
+#   r.add_field("PROVINCIA", "Jujuy")
+#   r.add_field("REGION", "NOA")
+#   r.add_field("NUMERO_DE_TRAMITE", "123")
+#   r.add_field("FECHA_DE_TRAMITE", "12/04/2014 15:00")
+#   r.add_field("DATOS_DEL_RESPONSABLE", "fruta")
+# end
 
-report.generate("./result/REMITO_INT_PRESENTA.odt")
+# report.generate("./result/REMITO_INT_PRESENTA.odt")
