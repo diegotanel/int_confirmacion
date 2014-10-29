c55c21674dc5ec1898553510d5d60d5584175eb3
Diego Tanel <diego.tanel@tecneg.com>
2014-10-28 21:11:36 -0300

    Se agrego la plantilla para el remito, se actualizao el archivo convocatiora_catalogo_2015.doc, se puso el texto de directores y técnicos como máximo 3 entre ambos

---
+++ app/models/generador_de_remito.rb
@@ -0,0 +1,14 @@
+require '../lib/odf-report'
+
+report = ODFReport::Report.new("./plantillas/REMITO_INT_PRESENTA.odt") do |r|
+
+  r.add_field("FECHA_DE_IMPRESION_DEL_REMITO", "14/04/2014 12:00")
+  r.add_field("NOMBRE_DEL_ESPECTACULO", "espectáculo fruta")
+  r.add_field("PROVINCIA", "Jujuy")
+  r.add_field("REGION", "NOA")
+  r.add_field("NUMERO_DE_TRAMITE", "123")
+  r.add_field("FECHA_DE_TRAMITE", "12/04/2014 15:00")
+  r.add_field("DATOS_DEL_RESPONSABLE", "fruta")
+end
+
+report.generate("./result/REMITO_INT_PRESENTA.odt")
