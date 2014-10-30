8b4869df8a7e1b9c04a2c7aa611102fc36c04a66
Martin <pereira.martin@gmail.com>
2014-10-30 12:15:48 -0300

    Se agrego persona juridica y comision directiva

--- app/controllers/personas_juridicas_controller.rb
+++ app/controllers/personas_juridicas_controller.rb
@@ -1,4 +1,55 @@
 class PersonasJuridicasController < ApplicationController
   before_action :inicializar_variables, only: [:new, :edit]
   before_action :signed_in_user, only: [:new, :show, :create, :edit, :update]
+
+  def new
+    @formulario = Formulario.find_by_id(params[:formulario_id])
+    @persona_juridica = PersonaJuridica.new
+  end
+
+  def show
+    @formulario = Formulario.find_by_id(params[:formulario_id])
+    @persona_juridica = PersonaJuridica.find(params[:id])
+  end
+
+  def create
+  	@formulario = Formulario.find_by_id(params[:formulario_id])
+  	@persona_juridica = @formulario.responsable.build_persona_juridica(persona_juridica_params)
+    if @persona_juridica.save
+      flash[:success] = "Datos de la persona juridica correctamente creados"
+      redirect_to formulario_persona_juridica_path(@formulario, @persona_juridica)
+    else
+      inicializar_variables
+      render 'new'
+    end
+  end
+
+  def edit
+  	@formulario = Formulario.find_by_id(params[:formulario_id])
+    @persona_juridica = PersonaJuridica.find(params[:id])
+  end
+
+  def update
+  	@formulario = Formulario.find_by_id(params[:formulario_id])
+    @persona_juridica = PersonaJuridica.find(params[:id])
+    if @persona_juridica.update_attributes(persona_juridica_params)
+      flash[:success] = "Datos de la persona juridica actualizados"
+      redirect_to formulario_persona_juridica_path(@formulario, @persona_juridica)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+	def persona_juridica_params
+	  params.require(:persona_juridica).permit(:nombre_per_juridica, :num_cuit, :num_per_juridica, :calle, 
+       :altura_calle, :localidad_id, :codigo_postal, :email_entidad, :tel_entidad)
+	end
+
+  def inicializar_variables
+    @provincias = Provincia.all
+    @region = Region.all
+    @localidades = Localidad.all
+  end
 end
--- app/controllers/responsables_controller.rb
+++ app/controllers/responsables_controller.rb
@@ -1,13 +1,33 @@
 class ResponsablesController < ApplicationController
-  before_action :signed_in_user, only: [:new, :show]
-
-	def new
-    @formulario = Formulario.find_by_id(params[:formulario_id])
-    @responsable = Responsable.new
-  end
+  before_action :signed_in_user, only: [:show, :edit, :update, :index]
 
   def show
     @formulario = Formulario.find_by_id(params[:formulario_id])
     @responsable = Responsable.find(params[:id])
+  end
+
+  def index
+    @formulario = Formulario.find_by_id(params[:formulario_id])
+    @formulario.create_responsable() unless @formulario.responsable
+    @fisica_existente = @formulario.responsable.persona_fisica_e
+    @fisica_nueva = @formulario.responsable.persona_fisica_n
+    @juridica = @formulario.responsable.persona_juridica
+    flash[:success] = "Datos del responsable creados"
+  end
+
+  def edit
+    @formulario = Formulario.find_by_id(params[:formulario_id])
+    @responsable = Responsable.find(params[:id])
+  end
+
+  def update
+    @formulario = Formulario.find_by_id(params[:formulario_id])
+    @responsable = Responsable.find(params[:id])
+    if @responsable.update_attributes(responsable_params)
+      flash[:success] = "Datos del responsable actualizados"
+      redirect_to formulario_responsable_path(@formulario, @responsable)
+    else
+      render 'edit'
+    end
   end
 end
