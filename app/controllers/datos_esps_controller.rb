91130182d382149c6a29ddc199276efc240fa467
Martin <pereira.martin21@gmail.com>
2014-10-29 23:16:49 -0300

    Se arreglaron cosas graficas

--- app/controllers/datos_esps_controller.rb
+++ app/controllers/datos_esps_controller.rb
@@ -43,8 +43,8 @@ class DatosEspsController < ApplicationController
   private
 
 	 def datos_esp_params
-	  params.require(:datos_esp).permit(:interpretes_escena, :directores_espectaculo, :fecha_de_estreno, :nombre_autor, 
-       :nacionalidad_autor, :duracion_espectaculo, :sinopsis_obra, :pagina_web, :facebook, :twitter, :foro, :blog, :youtube, :instagram, :gen_esp_ids => [], :publ_edad_ids => [], :publ_exp_ids => [], :formato_ids => [])
+	  params.require(:datos_esp).permit(:fecha_de_estreno, :nombre_autor, :nacionalidad_autor, :duracion_espectaculo, :sinopsis_obra, 
+      :pagina_web, :facebook, :twitter, :blog, :youtube, :instagram, :gen_esp_ids => [], :publ_edad_ids => [], :publ_exp_ids => [], :formato_ids => [])
 	 end
 
   def inicializar_variables
--- db/migrate/20141014190338_create_datos_esps.rb
+++ db/migrate/20141014190338_create_datos_esps.rb
@@ -6,13 +6,10 @@ class CreateDatosEsps < ActiveRecord::Migration
     	t.string :nombre_autor, :null => false
     	t.string :nacionalidad_autor, :null => false
     	t.string :duracion_espectaculo, :null => false
-    	t.string :interpretes_escena, :null => false
-    	t.string :directores_espectaculo, :null => false
     	t.string :sinopsis_obra, :null => false
     	t.string :pagina_web
     	t.string :facebook
     	t.string :twitter
-    	t.string :foro
     	t.string :blog
     	t.string :youtube
     	t.string :instagram
