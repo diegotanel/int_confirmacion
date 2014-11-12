9fbedca0571c36e2bc485e9018920a38e18193c7
Diego Tanel <dtanel@gmail.com>
2014-11-12 18:19:27 -0300

    Se cambio agrego decimales a ancho, alto y profundidad

--- app/models/datos_tec.rb
+++ app/models/datos_tec.rb
@@ -11,11 +11,11 @@ class DatosTec < ActiveRecord::Base
   validates :duracion_desmontaje, presence: true, unless: :saltear_validaciones_de_presencia
   validates :duracion_desmontaje, numericality: { only_integer: true }, allow_blank: true
   validates :ancho, presence: true, unless: :saltear_validaciones_de_presencia
-  validates :ancho, numericality: { only_integer: true }, allow_blank: true
+  validates :ancho, numericality: true, allow_blank: true
   validates :alto, presence: true, unless: :saltear_validaciones_de_presencia
-  validates :alto, numericality: { only_integer: true }, allow_blank: true
+  validates :alto, numericality: true, allow_blank: true
   validates :profundidad, presence: true, unless: :saltear_validaciones_de_presencia
-  validates :profundidad, numericality: { only_integer: true }, allow_blank: true
+  validates :profundidad, numericality: true, allow_blank: true
   validates :listado_artefactos_luminicos, presence: true, unless: :saltear_validaciones_de_presencia
   validates :listado_artefactos_luminicos, length: {maximum: 500}
   validates :listado_sonido, presence: true, unless: :saltear_validaciones_de_presencia
@@ -31,5 +31,20 @@ class DatosTec < ActiveRecord::Base
 
   has_attached_file :imagen, styles: { medium: '300x300>', thumb: '48x48>' }
   validates_attachment :imagen, :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", 'application/pdf'] },
-                                :size => { :less_than => 1.megabyte }
+    :size => { :less_than => 1.megabyte }
+
+  def ancho=(val)
+    val.sub!(',', '.') if val.is_a?(String)
+    self['ancho'] = val
+  end
+
+  def alto=(val)
+    val.sub!(',', '.') if val.is_a?(String)
+    self['alto'] = val
+  end
+
+  def profundidad=(val)
+    val.sub!(',', '.') if val.is_a?(String)
+    self['profundidad'] = val
+  end
 end
